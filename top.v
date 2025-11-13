b744bc642668fb67c65038b4d4be86f1555933ed
Daylenmathews <151976934+Daylenmathews@users.noreply.github.com>
2025-11-13 15:10:53 -0700

    Update top.v

--- top.v
+++ top.v
@@ -1,1 +1,29 @@
+module top(
+input btnU,
+input btnC,
+output [6:0]led
+);
+wire [2:0]ripple_q;
 
+ripple part_1(
+.clk(btnC),
+.reset(btnU),
+.q(ripple_q)
+);
+assign ripple_q[0] = led[0];
+assign ripple_q[1] = led[1];
+assign ripple_q[2] = led[2];
+
+wire [2:0]modulcount;
+wire modulout;
+modul part_2(
+.clk(btnC),
+.reset(btnU),
+.count(modulcount),
+.out(modulout)
+);
+assign modulcount[0] = led[3];
+assign modulcount[1] = led[4];
+assign modulcount[2] = led[5];
+assign modulout = led[6];
+endmodule
